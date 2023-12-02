45ff85102289b7e9aa344cdaf87ec89f9bea7f23
TY <taoyuan@users.noreply.github.com>
2023-12-02 16:24:00 +0800

    chore: update

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,7 +6,26 @@ module.exports = {
     {
       files: ['**/*.ts', '**/*.tsx'],
       rules: {
-        'simple-import-sort/imports': 'error',
+        'simple-import-sort/imports': [
+          'error',
+          {
+            groups: [
+              // Side effect imports.
+              ['^\\u0000'],
+              // Node.js builtins prefixed with `node:`.
+              ['^node:'],
+              // Packages.
+              // Things that start with a letter (or digit or underscore), or `@` followed by a letter.
+              ['^@?\\w'],
+              // Absolute imports and other imports such as Vue-style `@/foo`.
+              // Anything not matched in another group.
+              ['^'],
+              // Relative imports.
+              // Anything that starts with a dot.
+              ['^\\.'],
+            ],
+          },
+        ],
         'simple-import-sort/exports': 'error',
         'import/first': 'error',
         'import/newline-after-import': 'error',
